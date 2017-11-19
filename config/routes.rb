7fa427f3cfc3e295bcb94c0c4f38772fba149d1f
Alan Russell <ajrussellaudio@gmail.com>
2017-11-19 20:51:17 +0000

    scope routes to api prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :tours do
-    resources :points
+  scope :api do
+    resources :tours do
+      resources :points
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
