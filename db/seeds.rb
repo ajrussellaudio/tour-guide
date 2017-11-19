79fbb10501315ddbfb183fc85e9aec64fb407a11
Alan Russell <ajrussellaudio@gmail.com>
2017-11-19 18:45:29 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Point.delete_all
+Tour.delete_all
+
+tour = Tour.new({
+  name: "Glasgow to Edinburgh",
+  info: "A wee trip from the country's capital to its finest city."
+})
+tour.save
+
+Point.create([
+  {
+    name: "Edinburgh",
+    image_url: "http://edinburgh.org/media/743198/Cityscape.jpg",
+    info: "Scotland's capital. It's alright, I guess.",
+    latitude: 55.95206,
+    longitude: -3.19648,
+    tour: tour
+  }, {
+    name: "Glasgow",
+    image_url: "https://peoplemakeglasgow.com/images/Clydeside1.jpg",
+    info: "Dear green place. Second city of the Empire. The Big G. G-town. Home.",
+    latitude: 55.86515,
+    longitude: -4.25763,
+    tour: tour
+  }
+])
