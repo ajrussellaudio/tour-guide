ea9b22e129bd1f8de3895ceea73efa77178cc618
Alan Russell <ajrussellaudio@gmail.com>
2017-11-19 20:10:42 +0000

    alter routes to nest points inside tours

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :tours
-  resources :points
+  resources :tours do
+    resources :points
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
