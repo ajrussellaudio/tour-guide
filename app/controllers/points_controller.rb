4d4f894d57d2e6133568b2d31514a9353e72c192
Alan Russell <ajrussellaudio@gmail.com>
2017-11-19 20:14:14 +0000

    reduce points controller

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,51 +1,25 @@
 class PointsController < ApplicationController
-  before_action :set_point, only: [:show, :update, :destroy]
+  before_action :set_point, only: [:show]
 
   # GET /points
   def index
     @points = Point.all
 
-    render json: @points
+    render json: @points.as_json({
+      except: [:created_at, :updated_at]
+    })
   end
 
   # GET /points/1
   def show
-    render json: @point
-  end
-
-  # POST /points
-  def create
-    @point = Point.new(point_params)
-
-    if @point.save
-      render json: @point, status: :created, location: @point
-    else
-      render json: @point.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /points/1
-  def update
-    if @point.update(point_params)
-      render json: @point
-    else
-      render json: @point.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /points/1
-  def destroy
-    @point.destroy
+    render json: @point.as_json({
+      except: [:created_at, :updated_at]
+    })
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_point
       @point = Point.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def point_params
-      params.require(:point).permit(:info, :name, :image_url, :latitude, :longitude)
     end
 end
