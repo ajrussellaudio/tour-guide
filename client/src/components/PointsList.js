c967356bbe0aca1c2975368dedf706cde4dbe1a9
Alan Russell <ajrussellaudio@gmail.com>
2017-11-20 00:22:51 +0000

    add map link

--- client/src/components/PointsList.js
+++ client/src/components/PointsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Icon } from 'semantic-ui-react';
 
 export default (props) => {
   const cards = props.points.map(point => {
@@ -9,6 +9,12 @@ export default (props) => {
         <Card.Content>
           <Card.Header>{point.name}</Card.Header>
           <Card.Description>{point.info}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a href="#">
+            <Icon name="marker"/>
+            Map
+          </a>
         </Card.Content>
       </Card>
     )
