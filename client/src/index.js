ceb0e3e5722458085befc6b72d487401187c2a3d
Alan Russell <ajrussellaudio@gmail.com>
2017-11-21 13:30:07 +0000

    update index to use material ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.css';
 import './css/main.css';
-import './css/phone.css';
-import './css/desktop.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MaterialUiApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(
+  <MaterialUiApp />,
+  document.getElementById('root')
+);
